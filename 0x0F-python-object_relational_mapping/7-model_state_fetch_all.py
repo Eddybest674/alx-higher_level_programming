f2ecfbbad1d46099d6578bd7807b86840a5bcb86
Eddybest674 <edidiongeffiong056@gmail.com>
2022-09-09 16:47:00 -0700

    updated

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,25 +1,22 @@
 #!/usr/bin/python3
-'''script  for task 7'''
 
-from model_state import Base, State
+'''
+    Lists all state objs.
+'''
+
+import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-import sys
+from sys import argv
+from model_state import Base, State
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-    host = 'localhost'
-    port = '3306'
-    engine = create_engine('mysql+mysqldb://{}:{}@{}:{}/{}'.format(
-             username, password, host, port, db_name), pool_pre_ping=True)
-    Session = sessionmaker(bind=engine)
-    local_session = Session()
-    states = local_session.query(State).order_by(State.id).all()
-    local_session.close()
-    engine.dispose()
-
-    for state in states:
-        print(str(state.id) + ': ' + state.name)
-
+    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
+                                                                    argv[2],
+                                                                    argv[3]))
+    Base.metadata.create_all(eng)
+    Session = sessionmaker(bind=eng)
+    session = Session()
+    for state in session.query(State).order_by(State.id):
+        print("{}: {}".format(state.id, state.name))
+    session.close()
--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,29 +1,25 @@
 #!/usr/bin/python3
-'''script for task 8'''
 
-from model_state import Base, State
+'''
+    Lists first state from DB.
+'''
+
+import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.pool import NullPool
-import sys
+from sys import argv
+from model_state import Base, State
 
-
-if __name__ == '__main__':
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-    host = 'localhost'
-    port = '3306'
-
-    engine = create_engine('mysql+mysqldb://{}:{}@{}:{}/{}'.format(
-                           username, password, host, port, db_name),
-                           pool_pre_ping=True, poolclass=NullPool)
-    Session = sessionmaker(bind=engine)
-    local_session = Session()
-    result = local_session.query(State).order_by(State.id).first()
-    local_session.close()
-
-    if result:
-        print('{}: {}'.format(result.id, result.name))
+if __name__ == "__main__":
+    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
+                                                                    argv[2],
+                                                                    argv[3]))
+    Base.metadata.create_all(eng)
+    Session = sessionmaker(bind=eng)
+    session = Session()
+    first_state = session.query(State).order_by(State.id).first()
+    if first_state is not None:
+        print("{}: {}".format(first_state.id, first_state.name))
     else:
-        print('Nothing')
+        print("Nothing")
+    session.close()
